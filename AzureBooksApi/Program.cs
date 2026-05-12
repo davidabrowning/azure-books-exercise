1f4b44e6d90b8a63ccea0377355caa8a014fd3cb
David Browning <david.a.browning@gmail.com>
2026-05-12 15:11:51 +0200

    Register AppDbContext

--- AzureBooksApi/Program.cs
+++ AzureBooksApi/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
 
         builder.Services.AddOpenApi();
 
